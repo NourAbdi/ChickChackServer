1dffd265538d25c0fe3ab75292eb559160212c01
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-05-14 17:05:29 +0300

    sda
    sad

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 const { payRequest } = require("./pay");
-const { getUsers } = require("./users");
+const { getUsers, addUser } = require("./users");
 
 const { Client } = require("@googlemaps/google-maps-services-js");
 // const stripeClient = require("stripe")(functions.config().stripe.key);
@@ -21,13 +21,11 @@ exports.pay = functions.https.onRequest((request, response) => {
 });
 
 exports.users = functions.https.onRequest((request, response) => {
-  getUsers(request, response);
+  if (request.method === "GET") {
+    getUsers(request, response);
+  } else if (request.method === "POST") {
+    addUser(request, response);
+  } else {
+    response.status(405).send("Method Not Allowed");
+  }
 });
-
-
-
-
-
-
-
-
--- functions/users/index.js
+++ functions/users/index.js
@@ -1,46 +1,43 @@
 const url = require("url");
 const functions = require("firebase-functions");
-
-const admin = require('firebase-admin');
-const serviceAccount = require('./service_account.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./service_account.json");
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // Get a reference to your Firestore database
 const db = admin.firestore();
 
-// Now you can use the Firebase Firestore API to read and write data to your database
-const collectionRef = db.collection('my-collection');
-collectionRef.add({
-    name: 'John Doe',
-    age: 30
-})
-    .then(docRef => {
-        console.log(`Document written with ID: ${docRef.id}`);
+module.exports.addUser = (uid, email) => {
+  const userRef = db.collection("users").doc(uid);
+  userRef
+    .set({
+      email,
     })
-    .catch(error => {
-        console.error('Error adding document:', error);
+    .then(() => {
+      console.log(`User with uid: ${uid} added to the database`);
+    })
+    .catch((error) => {
+      console.error("Error adding user to database:", error);
     });
-
-
-
+};
 
 module.exports.getUsers = (request, response) => {
-    const usersRef = db.collection('users');
-    usersRef.get()
-        .then(snapshot => {
-            const users = [];
-            snapshot.forEach(doc => {
-                users.push(doc.data());
-            });
-            response.send(users);
-        })
-        .catch(error => {
-            console.error('Error getting users:', error);
-            response.status(500).send('Error getting users');
-        });
-
+  const usersRef = db.collection("users");
+  usersRef
+    .get()
+    .then((snapshot) => {
+      const users = [];
+      snapshot.forEach((doc) => {
+        users.push({ id: doc.id, ...doc.data() });
+      });
+      response.send(users);
+    })
+    .catch((error) => {
+      console.error("Error getting users:", error);
+      response.status(500).send("Error getting users");
+    });
 };
