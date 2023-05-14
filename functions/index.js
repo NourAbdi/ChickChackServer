c112427e676f0223b247db756c8d87fdba21b805
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-05-14 18:15:03 +0300

    sa
    sa

--- functions/index.js
+++ functions/index.js
@@ -12,13 +12,13 @@ exports.geocode = functions.https.onRequest((request, response) => {
   geocodeRequest(request, response);
 });
 
-exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response);
-});
+// exports.placesNearby = functions.https.onRequest((request, response) => {
+  // placesRequest(request, response);
+// });
 
-exports.pay = functions.https.onRequest((request, response) => {
+// exports.pay = functions.https.onRequest((request, response) => {
   // payRequest(request, response, stripeClient);
-});
+// });
 
 exports.users = functions.https.onRequest((request, response) => {
   addUser(request, response);
