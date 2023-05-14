55012f7b936053717daccfefdc85349a121a4801
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-05-14 18:45:25 +0300

    asd
    asd

--- functions/users/index.js
+++ functions/users/index.js
@@ -4,17 +4,18 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addUser = functions.auth.user().onCreate(async (user) => {
-  try {
-    const { uid, email } = user;
-    const userData = {
-      email,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    };
-    const userDocRef = admin.firestore().collection("users").doc(uid);
-    await userDocRef.set(userData);
-    return true;
-  } catch (error) {
-    console.error("Error adding user data", error);
-    return false;
-  }
-});
+    try {
+      const { uid, email } = user;
+      const userData = {
+        email,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      };
+      const userDocRef = admin.firestore().collection("users").doc(uid);
+      await userDocRef.set(userData);
+      return true;
+    } catch (error) {
+      console.error("Error adding user data", error);
+      return false;
+    }
+  });
+  
