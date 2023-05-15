94b718a9e8752893d400ba27f29d81973860ae24
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-05-15 15:56:47 +0300

    f

--- functions/users/index.js
+++ functions/users/index.js
@@ -26,34 +26,37 @@ exports.getUsers = (request, response) => {
 };
 
 exports.addUser = (request, response) => {
-    console.log("gggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
     const { email, role } = request.query;
-    console.log( email, role);
-    admin.auth().getUserByEmail(email)
-        .then(async userRecord => {
-            const uid = userRecord.uid;
-            const usersRef = db.collection('users');
-            const doc = usersRef.doc(uid);
-            const snapshot = await doc.get();
-            if (snapshot.exists) {
-                const message = `User with email ${email} already exists`;
-                console.warn(message);
-                response.status(400).send({ success: false, message });
-            } else {
-                const { displayName } = userRecord;
-                const userData = {
-                    email,
-                    role: role || 'client', // Set default role to "client" if role parameter is not provided
-                    ...(displayName && { name: displayName })
-                };
-                await doc.set(userData);
-                const message = `User with email ${email} and role ${userData.role} added successfully`;
-                console.log(message);
-                response.send({ success: true, message });
-            }
-        })
-        .catch(error => {
-            console.error('Error adding user:', error);
-            response.status(500).send({ success: false, message: 'Error adding user' });
-        });
-};
+    const uid = request.user.uid; // Get the uid from the Firebase Auth token
+    const usersRef = db.collection('users');
+    const doc = usersRef.doc(uid);
+    doc.get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          const message = `User with email ${email} already exists`;
+          console.warn(message);
+          response.status(400).send({ success: false, message });
+        } else {
+          const userData = {
+            email,
+            role: role || 'client', // Set default role to "client" if role parameter is not provided
+            uid // Include the uid in the user data
+          };
+          doc.set(userData)
+            .then(() => {
+              const message = `User with email ${email} and role ${userData.role} added successfully`;
+              console.log(message);
+              response.send({ success: true, message });
+            })
+            .catch((error) => {
+              console.error('Error adding user:', error);
+              response.status(500).send({ success: false, message: 'Error adding user' });
+            });
+        }
+      })
+      .catch((error) => {
+        console.error('Error adding user:', error);
+        response.status(500).send({ success: false, message: 'Error adding user' });
+      });
+  };
+  
